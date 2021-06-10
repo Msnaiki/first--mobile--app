1395e59df4ae8e75b829c01b8c180ad2d9cc6b0f
mehdi snaiki <mehdisnaikidono2@gmail.com>
2021-06-10 15:49:14 +0100

    activity

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private  Button button;
+    private EditText editText;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.valbutton);
+        editText= (EditText) findViewById(R.id.field1);
+        textView = (TextView) findViewById(R.id.textView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String NameStored = editText.getText().toString();
+
+                textView.setText(NameStored);
+            }
+        });
+    }
+}
